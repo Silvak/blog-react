de41d627bcff596057da7de8ccdbaf1c0c08efd1
silvak <silvak.jeg@gmail.com>
2022-04-08 22:20:43 -0400

    query and logic to commentForm

--- services/index.js
+++ services/index.js
@@ -124,3 +124,16 @@ export const getPostDetailts = async (slug) => {
 const result = await request(graphqlAPI, query, {slug});
 return result?.post;
 }
+
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
