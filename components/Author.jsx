2312f204a2c51479760299a943baee4d86b539ed
silvak <silvak.jeg@gmail.com>
2022-04-16 15:47:58 -0400

    fixed carousel arrows

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,17 +4,16 @@ import Image from 'next/image'
 
 const Author = ({author}) => {
   return (
-    <div className='relative border border-[#fef0e5] mt-12 mb-12'
+    <div className='mt-8 mb-12 border border-[#fef0e5]'
     style={{ backgroundImage: `url('https://images.vexels.com/media/users/3/157944/isolated/preview/b6a72d46f16e457ccfacf410edc462a6-dots-grid-design.png')` }}
     >
-      <div className='flex justify-around p-2 m-8 lg:m-14 bg-[#fef0e5]'>
+      <div className='flex p-2 m-8 lg:m-14 bg-[#fef0e5]'>
         <div className='w-20 h-20  p-2 border border-[#0f1630]' style={{minWidth:"64px"}}>
-          <img
-            alt={author.name}
-            src={author.photo.url}
-            className='object-containt align-middle'
-          />
-          
+            <img
+              alt={author.name}
+              src={author.photo.url}
+              className='object-containt align-middle'
+            /> 
         </div>
         <p className='m-auto pl-4 text-sm lg:text-md'>
           <span className='text-left text-lg font-bold block mb-2'>
@@ -23,8 +22,6 @@ const Author = ({author}) => {
           {author.bio}
         </p>
       </div>
-      
-      
     </div>
   )
 }
